c473f0dfb032fd283a931f29e48e9c7694f4d3e2
Corne <threalcorne@gmail.com>
2021-01-26 09:34:46 +0200

    From laptop

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WebScraper',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='free',
+    author='Corne',
+    author_email='therealcorne@gmail.com',
+    description='My first webscraper'
+)
